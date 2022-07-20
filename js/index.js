95ebe8d5216c66b814b4052080566fffc7f8d3b2
Jordane <jordan2offshel@hotmaile.com>
2022-07-19 20:10:56 -0400

    Commit

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@ var tl;
 tl = gsap.timeline({defauts: {ease: "power1.out"}});
 
 tl.to(".content .desc", {opacity: 1, x: "0%", duration: .5, stagger: .25 });
+tl.to(".humbger", {opacity: 1, x: "0%", duration: .5, stagger: .25 });
 tl.to(".content .desc .btn", {opacity: 1, y: "0%", duration: .5, stagger: .25 });
 tl.to(".content .main-images", {opacity: 1, x: "0%", duration: .5, stagger: .25 });
 tl.to(".logo a", {opacity: 1, y: "0%", duration: .5, stagger: .25 });
 tl.to(".navb .navb-link", {opacity: 1, y: "0%", duration: .5, stagger: .25 });
+tl.to(".container-s ul li", {opacity: 1, y: "0%", duration: .5, stagger: .25 });
 
 var navb = document.querySelector(".js-header");
 
